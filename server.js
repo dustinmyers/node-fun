560ddd9f8098e7cc37bd2a10b1cc6c2aa9c4858c
Dustin Myers <dustint314@gmail.com>
2015-08-10 19:24:00 +0000

    starting with twilio

--- server.js
+++ server.js
@@ -3,9 +3,39 @@
 ////////////////////////////
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
+var twilio = require('twilio');
+
 
 var app = express();
 var port = 3033;
+
+////////////////////////////
+///     MIDDLEWARE     
+////////////////////////////
+app.use(express.static('public'));
+// app.use(bodyParser());    ------normal
+app.use(bodyParser.urlencoded({             
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+var message = {
+    "message": "HELLO WORLD!!!"
+};
+////////////////////////////
+///     ENDPOINTS     
+////////////////////////////
+app.get('/api/message', function(req, res) {
+    return res.json(message);
+});
+app.post('/api/send_text_message', function(req, res) {
+    console.log(req.body.message);
+    res.send();
+})
+
 
 
 // app.listen(port, function() {
